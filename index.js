6f981f858b2a381d30497612c53cfa8f3bc3b434
Tej Patel <tejpatel317@gmail.com>
2022-05-11 14:20:38 -0400

    commit tree 95e5bef76b4c811771ec110b403e0d8c8b504e12

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let startingCounter = 0;
+  let endingCounter = word.length-1;
+  let value = true;
+  let stopValue = (word.length-1)/2
+  for (startingCounter=0; startingCounter<stopValue; startingCounter++) {
+    if (word.charAt(startingCounter) === word.charAt(endingCounter)) {
+      value = true;
+    }
+    else {
+      value = false;
+      return value
+    }
+    endingCounter--
+  }
+  return value
 }
 
 /* 
-  Add your pseudocode here
+  Write a function with takes a word as a string
+  Set starting counter to 0.
+  Set ending counter to lenth of the string minus 1
+  Set boolean equal to true
+  Write a for loop which continues as long as the starting counter is half the length of the string minus 1
+    -> We use half the length because we need to test only to the half way point of the word
+    -> If the word has an odd number of letters, the middle letter does not matter.
+    -> If the word has an even number of letters, the loop will check every letter. 
+  The for loop has an if statement which will return true if the character at the starting counter is equal to the character at the ending counter.
+  Else, the boolean will equal false
+The starting counter will be incremented by one
+The ending counter will be decremented by one 
 */
 
 /*
